11f368274d5555b040ea482f787c415162650571
CristyMejia927 <44482863+cristinamejia1402@users.noreply.github.com>
2021-05-26 21:49:22 -0500

    Create calculoMayor.py

---
+++ calculoMayor.py
@@ -0,0 +1,12 @@
+#pedirle al usuario dos valores numéricos. Imprimir el mayor de ellos.
+
+valor1 = float(input("Ingrese el valor #1"))
+valor2 = float(input("Ingrese el valor #2"))
+
+if valor1 > valor2:
+    print("el primer dato es mayor que el segundo")
+elif valor2 > valor1:
+    print("el segundo dato es mayor que el primero")
+else:
+    print("Los valores son iguales. No hay ninguno mayor que el otro")
+© 2021 GitHub, Inc.
