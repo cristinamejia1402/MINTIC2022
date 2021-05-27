2cb92664d8008dafdf8ef945946e68aed8f507b7
CristyMejia927 <44482863+cristinamejia1402@users.noreply.github.com>
2021-05-27 03:55:25 -0500

    Create 3-3conversionDeCaF.py

---
+++ 3-3conversionDeCaF.py
@@ -0,0 +1,9 @@
+#Paso1: pedir los °C
+valorCentigrados = int(input("Por favor ingrese el valor en °C: "))
+
+#Paso2: Aplicar fórmula de conversión entre °C y F
+valorFahrenheit = (valorCentigrados * 9/5) + 32
+
+#Paso3: mostrar el resultado
+print(f'El valor en Fahrenheit es: {valorFahrenheit}')
+© 2021 GitHub, Inc.
